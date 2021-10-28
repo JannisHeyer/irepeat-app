d662dda78226583f236cd519718c8057dbf4f50c
JannisHeyer <jbr.heyer@outlook.de>
2021-10-28 20:35:23 +0200

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>iRepeat</h1>
+      </div>
+    </>
+  );
+}
