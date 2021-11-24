7f5e7676c39fa6863843e3bcdd3f50ef13273dc4
JannisHeyer <90189831+JannisHeyer@users.noreply.github.com>
2021-11-24 22:02:26 +0100

    Delete dataBase.js

--- utils/dataBase.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-const client = new MongoClient(process.env.DB_URL, options);
-
-export default client;
